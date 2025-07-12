c92e662ac2fe7cfdb217effa4448aed9108026c1
Nozhan A. <nozhana@gmail.com>
2025-07-12 22:46:59 +0330

    More tree initializers

--- Novelty/DataTypes/Tree.swift
+++ Novelty/DataTypes/Tree.swift
@@ -28,13 +28,73 @@ struct Tree<T> {
 extension Tree {
     static func from(_ value: T, children: KeyPath<T, [T]>) -> Self {
         Tree(value, children: value[keyPath: children].map { child in
-            Tree.from(child, children: children)
+            from(child, children: children)
         })
+    }
+    
+    static func from(_ value: T, children childrenGenerator: @escaping (T) -> [T]) -> Self {
+        Tree(value, children: childrenGenerator(value).map { child in
+            from(child, children: childrenGenerator)
+        })
+    }
+    
+    static func from(_ value: T, children childrenGenerator: @escaping (T) async throws -> [T]) async rethrows -> Self {
+        var children = [Tree<T>]()
+        for child in try await childrenGenerator(value) {
+            let tree = try await from(child, children: childrenGenerator)
+            children.append(tree)
+        }
+        return Tree(value, children: children)
+    }
+}
+
+extension Tree {
+    func child(_ child: T, childBuilder: @escaping (Self) -> Self) -> Self {
+        Tree(value, children: children + [childBuilder(Tree(child))])
+    }
+    
+    func child(_ child: T) -> Self {
+        Tree(value, children: children + [Tree(child)])
     }
 }
 
 extension Tree: Decodable where T: Decodable {}
 extension Tree: Encodable where T: Encodable {}
+
+typealias TreeDictionaryLiteral<T> = (T, [Tree<T>])
+
+extension Tree: ExpressibleByDictionaryLiteral {
+    init(dictionaryLiteral elements: TreeDictionaryLiteral<T>...) {
+        let element = elements.first!
+        self.init(element.0, children: element.1)
+    }
+}
+
+extension Tree: ExpressibleByIntegerLiteral where T == Int {
+    init(integerLiteral value: Int) {
+        self.init(value)
+    }
+}
+
+extension Tree: ExpressibleByFloatLiteral where T == Float {
+    init(floatLiteral value: Float) {
+        self.init(value)
+    }
+}
+
+extension Tree: ExpressibleByUnicodeScalarLiteral where T == String {}
+extension Tree: ExpressibleByExtendedGraphemeClusterLiteral where T == String {}
+extension Tree: ExpressibleByStringLiteral where T == String {
+    init(stringLiteral value: String) {
+        self.init(value)
+    }
+}
+
+extension Tree: ExpressibleByBooleanLiteral where T == Bool {
+    init(booleanLiteral value: Bool) {
+        self.init(value)
+    }
+}
 
 @resultBuilder
 struct TreeBuilder<T> {
